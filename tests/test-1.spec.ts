108e846c03f27f9b6125976b6acf6d9367dfad04
Artem <tiomixx123@gmail.com>
2025-10-29 22:34:42 +0200

    added something

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.locator('[data-test="login-credentials"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="username"]').press("Tab");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await page.locator('[data-test="item-sauce-labs-backpack-img"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('[data-test="item-3-img-link"]').click();
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.getByText("Swag Labs").click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill("Valenor");
+  await page.locator('[data-test="firstName"]').press("Tab");
+  await page.locator('[data-test="lastName"]').fill("Ronalev");
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill("12341");
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+});
